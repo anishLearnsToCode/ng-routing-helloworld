f3ef33fd5dcab50b95803f64928a126665a9111a
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-05-01 23:01:55 +0200

    Adds authorization service

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,8 +1,12 @@
 import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
-  
+export class AuthGuardService implements CanActivate {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return undefined;
+  }
 }
